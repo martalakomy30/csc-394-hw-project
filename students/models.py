db7734ba3daf71c0c6682d2452fde9ad08199aa2
martalakomy30 <martalakomy30@gmail.com>
2023-01-29 20:22:17 -0600

    HW9 step 1 done

--- students/models.py
+++ students/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 # Create your models here.    
+
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    
+        
     def __str__(self) -> str:
         return self.first_name + " " + self.last_name
-
+    
+class Course(models.Model):
+    department = models.CharField(max_length=60)
+    number = models.CharField(max_length=3)
+    students = models.ManyToManyField(Student)
+    
+    def __str__(self) -> str:
+        return self.department + "-" + self.number
