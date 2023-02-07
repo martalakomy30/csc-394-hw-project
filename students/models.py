4601b9be8a8d65205f0f74619f5879bbe27b70fd
martalakomy30 <martalakomy30@gmail.com>
2023-02-07 16:27:35 -0600

    perf

--- students/models.py
+++ students/models.py
@@ -2,17 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.    
 
-class Student(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
-    def __str__(self) -> str:
-        return self.first_name + " " + self.last_name
+#class Student(models.Model):
+#    first_name = models.CharField(max_length=30)
+#    last_name = models.CharField(max_length=30)
+#    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
+#    def __str__(self) -> str:
+#        return self.first_name + " " + self.last_name
+#    
+#class Course(models.Model):
+#    department = models.CharField(max_length=60)
+#    number = models.CharField(max_length=3)
+#    students = models.ManyToManyField(Student)
+ 
     
-class Course(models.Model):
-    department = models.CharField(max_length=60)
-    number = models.CharField(max_length=3)
-    students = models.ManyToManyField(Student, related_name='student_courses')
-    
+#    def __str__(self) -> str:
+#       return self.department + "-" + self.number
+
+class Perfume(models.Model):
+    perfume_name = models.CharField(max_length=60)
+    brand_name = models.CharField(max_length=60)
+    students = models.ManyToManyField(Student)    
     def __str__(self) -> str:
-        return self.department + "-" + self.number
+        return self.perfume_name + " by " + brand_name
